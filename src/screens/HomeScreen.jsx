accdf8bcdefbdb3129ab873bbcda7dc4dbbd8265
Hssnurrahman <54182451+Hssnurrahman@users.noreply.github.com>
2021-12-21 21:55:34 +0500

    Product Pagination Added!

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -6,17 +6,21 @@ import { useSelector, useDispatch } from "react-redux";
 import { fetchProducts } from "../actions/productActions";
 import Loading from "../components/Loading";
 import Message from "../components/Message";
+import { useParams } from "react-router-dom";
+import Paginate from "../components/Paginate";
 
 const HomeScreen = () => {
+  let { keyword, pageNumber } = useParams();
+
   const dispatch = useDispatch();
 
   const productsList = useSelector((state) => state.productList);
 
-  const { products, error, loading } = productsList;
+  const { products, error, loading, page, pages } = productsList;
 
   useEffect(() => {
-    dispatch(fetchProducts());
-  }, [dispatch]);
+    dispatch(fetchProducts(keyword, pageNumber));
+  }, [dispatch, keyword, pageNumber]);
 
   return (
     <>
@@ -24,13 +28,16 @@ const HomeScreen = () => {
       {loading && <Loading />}
       {error && <Message variant="danger">{error}</Message>}
       {!loading && !error && (
-        <Row>
-          {products.map((product) => (
-            <Col key={product._id} sm={12} md={4}>
-              <Product product={product} />
-            </Col>
-          ))}
-        </Row>
+        <>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={4}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+          <Paginate pages={pages} page={page} keyword={keyword && keyword} />
+        </>
       )}
     </>
   );
